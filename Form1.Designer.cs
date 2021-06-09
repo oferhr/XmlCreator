ab8390e57d220b4807a5c9d31afa0b1a26da5679
Ofer Harari <offa23@hotmail.com>
2021-06-09 16:58:56 +0000

    Version 1.0.0
    git-tfs-id: [https://oferh.visualstudio.com/DefaultCollection]$/Projects/XmlCreator;C158

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace XmlCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.butDest = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnArchive = new System.Windows.Forms.Button();
+            this.txtArchive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRow = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblOf = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(579, 22);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 10;
+            this.btnBrowse.Text = "חפש";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(61, 24);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(495, 20);
+            this.txtMain.TabIndex = 9;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lbl1.Location = new System.Drawing.Point(662, 26);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(95, 19);
+            this.lbl1.TabIndex = 8;
+            this.lbl1.Text = "תקייה ראשית";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(617, 240);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "התחל";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // butDest
+            // 
+            this.butDest.Location = new System.Drawing.Point(579, 71);
+            this.butDest.Name = "butDest";
+            this.butDest.Size = new System.Drawing.Size(75, 23);
+            this.butDest.TabIndex = 14;
+            this.butDest.Text = "חפש";
+            this.butDest.UseVisualStyleBackColor = true;
+            this.butDest.Click += new System.EventHandler(this.butDest_Click);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(61, 71);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(495, 20);
+            this.txtDest.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(668, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "תקיית יעד";
+            // 
+            // btnArchive
+            // 
+            this.btnArchive.Location = new System.Drawing.Point(579, 128);
+            this.btnArchive.Name = "btnArchive";
+            this.btnArchive.Size = new System.Drawing.Size(75, 23);
+            this.btnArchive.TabIndex = 17;
+            this.btnArchive.Text = "חפש";
+            this.btnArchive.UseVisualStyleBackColor = true;
+            this.btnArchive.Click += new System.EventHandler(this.btnArchive_Click);
+            // 
+            // txtArchive
+            // 
+            this.txtArchive.Location = new System.Drawing.Point(61, 128);
+            this.txtArchive.Name = "txtArchive";
+            this.txtArchive.Size = new System.Drawing.Size(495, 20);
+            this.txtArchive.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(668, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "תקיית ארכיון";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "סגור";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(88, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Working on row";
+            // 
+            // lblRow
+            // 
+            this.lblRow.AutoSize = true;
+            this.lblRow.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblRow.Location = new System.Drawing.Point(212, 193);
+            this.lblRow.Name = "lblRow";
+            this.lblRow.Size = new System.Drawing.Size(16, 16);
+            this.lblRow.TabIndex = 20;
+            this.lblRow.Text = "x";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(247, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Of";
+            // 
+            // lblOf
+            // 
+            this.lblOf.AutoSize = true;
+            this.lblOf.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblOf.Location = new System.Drawing.Point(294, 193);
+            this.lblOf.Name = "lblOf";
+            this.lblOf.Size = new System.Drawing.Size(16, 16);
+            this.lblOf.TabIndex = 22;
+            this.lblOf.Text = "x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOf);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblRow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnArchive);
+            this.Controls.Add(this.txtArchive);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.butDest);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtMain);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button butDest;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnArchive;
+        private System.Windows.Forms.TextBox txtArchive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblOf;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,354 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Configuration;
+
+namespace XmlCreator
+{
+    public partial class Form1 : Form
+    {
+        enum NameType
+        {
+            XML,
+            PDF_VSR,
+            PD_YIP
+        }
+        private string Code_Vsr = string.Empty;
+        private string Code_Yip = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var dirmPath = Properties.Settings.Default.MainPath;
+            if (!string.IsNullOrEmpty(dirmPath))
+            {
+                txtMain.Text = dirmPath;
+            }
+            var dirtPath = Properties.Settings.Default.DestPath;
+            if (!string.IsNullOrEmpty(dirtPath))
+            {
+                txtDest.Text = dirtPath;
+            }
+            var diraPath = Properties.Settings.Default.ArchivePath;
+            if (!string.IsNullOrEmpty(diraPath))
+            {
+                txtArchive.Text = diraPath;
+            }
+            Code_Vsr = ConfigurationManager.AppSettings.Get("Code_Vsr");
+            Code_Yip = ConfigurationManager.AppSettings.Get("Code_Yip");
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new OpenFileDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.FileName))
+                {
+                    txtMain.Text = fbd.FileName;
+                    Properties.Settings.Default.MainPath = fbd.FileName;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var dirName = DateTime.Now.ToString("yyyyMMddHHmmss");
+            var archivePath = Path.Combine(txtArchive.Text, dirName);
+            var mainDir = Path.GetDirectoryName(txtMain.Text);
+            var excelPath = Path.Combine(archivePath, Path.GetFileName(txtMain.Text));
+            Directory.Move(mainDir, archivePath);
+            Directory.CreateDirectory(mainDir);
+            ParseExcel(archivePath, excelPath, dirName);
+        }
+        private void ParseExcel(string archivePath, string excelPath, string dirName)
+        {
+            var list = new List<string>();
+            var rowCounter = 0;
+            var xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = null;
+            Excel._Worksheet xlWorksheet = null;
+            Excel.Range xlRange = null;
+            int i;
+            string vsrname = string.Empty;
+            string id = string.Empty;
+            try
+            {
+
+                xlWorkbook = xlApp.Workbooks.Open(excelPath);
+                xlApp.Visible = false;
+                xlWorksheet = (Excel._Worksheet)xlWorkbook.Sheets[1];
+                var lastRow = xlWorksheet.Cells.SpecialCells(XlCellType.xlCellTypeLastCell, Type.Missing).Row;
+                lblOf.Text = (lastRow - 1).ToString();
+                lblRow.Text = "0";
+                System.Windows.Forms.Application.DoEvents();
+                for (i = 2; i <= lastRow; i++)
+                {
+                    try
+                    {
+                        if (xlWorksheet.Range["A" + i, "A" + i].Value2 == null)
+                        {
+                            continue;
+                        }
+                        for (var XmlCounter = 1; XmlCounter <= 2; XmlCounter++)
+                        {
+                            var clientFile = xlWorksheet.Range["A" + i, "A" + i].Value2.ToString();
+                            var letter = xlWorksheet.Range["B" + i, "B" + i].Value2.ToString();
+                            var OrderCompanyID = clientFile + letter + XmlCounter;
+                            var FirstName = xlWorksheet.Range["C" + i, "C" + i].Value2.ToString();
+                            var LastName = xlWorksheet.Range["D" + i, "D" + i].Value2.ToString();
+                            id = xlWorksheet.Range["E" + i, "E" + i].Value2.ToString();
+                            var tickDate = xlWorksheet.Range["F" + i, "F" + i].Value2?.ToString();
+
+
+                            vsrname = CreateName(OrderCompanyID, id, "pdf", NameType.PDF_VSR);
+                            var yipname = CreateName(OrderCompanyID, id, "pdf", NameType.PD_YIP);
+                            var xmlname = CreateName(OrderCompanyID, id, "xml", NameType.XML);
+                            var type = XmlCounter == 1 ? "201" : "205";
+                            string stickdate = null;
+                            if (!string.IsNullOrEmpty(tickDate))
+                            {
+                                var dblTd = double.Parse(tickDate);
+                                stickdate = DateTime.FromOADate(dblTd).ToString("yyyy-MM-ddTHH\\:mm\\:ss.mmm");
+                            }
+
+
+
+                            var pdvsr = Code_Vsr + "_" + id;
+                            var pdyip = id + "_" + Code_Yip;
+                            var vsrpath = string.Empty;
+                            var yippath = string.Empty;
+                            var newVsrPath = string.Empty;
+                            var newYipPath = string.Empty;
+
+                            string[] files = Directory.GetFiles(archivePath);
+                            foreach (var file in files)
+                            {
+                                var nm = Path.GetFileNameWithoutExtension(file);
+                                if (nm.Contains(pdvsr))
+                                {
+                                    vsrpath = file;
+                                }
+
+                                var parts = nm.Split('_');
+                                if (Array.Exists(parts, f => f == Code_Yip) && Array.Exists(parts, f => f == id))
+                                {
+                                    yippath = file;
+                                }
+                            }
+                            if (File.Exists(vsrpath) && File.Exists(yippath))
+                            {
+                                newVsrPath = Path.Combine(archivePath, vsrname);
+                                newYipPath = Path.Combine(archivePath, yipname);
+                                File.Copy(vsrpath, newVsrPath);
+                                File.Copy(yippath, newYipPath);
+                                if (XmlCounter == 2)
+                                {
+                                    File.Delete(vsrpath);
+                                    File.Delete(yippath);
+                                }
+                            }
+                            else
+                            {
+                                lblRow.Text = (i-1).ToString();
+                                System.Windows.Forms.Application.DoEvents();
+                                if (!File.Exists(vsrpath))
+                                {
+                                    throw(new Exception("קובץ וסר לא קיים" + " --- " + "שורה" + "  " + i));
+                                }
+                                if (!File.Exists(yippath))
+                                {
+                                    throw (new Exception("קובץ יפוי כח לא קיים" + " --- " + "שורה" + "  " + i));
+                                }
+                                
+                            }
+                            id = id.PadLeft(9, '0');
+                            var sts = new XmlWriterSettings()
+                            {
+                                Indent = true
+                            };
+
+                            using (var writer = XmlWriter.Create(Path.Combine(archivePath, xmlname), sts))
+                            {
+                                writer.WriteStartDocument();
+                                writer.WriteStartElement("ActivityData");
+                                writer.WriteStartElement("SPDataSetResults");
+                                writer.WriteStartElement("SystemID");
+                                writer.WriteString("2");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("SystemName");
+                                writer.WriteString("חברות הביטוח");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("CompanyID");
+                                writer.WriteString("5");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("CompanyName");
+                                writer.WriteString("ערן מור");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("Interface");
+                                writer.WriteString(type);
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("ServiceID");
+                                writer.WriteString("10");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("OrderBtlID");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("OrderCompanyID");
+                                writer.WriteString(OrderCompanyID);
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("TimeStamp");
+                                writer.WriteString(DateTime.Now.ToString("yyyy-MM-ddTHH\\:mm\\:ss.mmm"));
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("Zehut");
+                                writer.WriteString(id);
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("FirstName");
+                                writer.WriteString(FirstName);
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("LastName");
+                                writer.WriteString(LastName);
+                                writer.WriteEndElement();
+                                if (!string.IsNullOrEmpty(stickdate))
+                                {
+                                    writer.WriteStartElement("TikDate");
+                                    writer.WriteString(stickdate);
+                                    writer.WriteEndElement();
+                                }
+                                writer.WriteStartElement("LastName");
+                                writer.WriteString(LastName);
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("KodGimla");
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("vasarDate");
+                                //writer.WriteString(DateTime.Now.ToString("yyyy-MM-ddTHH\\:mm\\:ss"));
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("attachmentFile");
+                                writer.WriteStartElement("FileName");
+                                writer.WriteString(vsrname);
+                                writer.WriteEndElement();
+                                writer.WriteEndElement();
+                                writer.WriteStartElement("attachmentFile");
+                                writer.WriteStartElement("FileName");
+                                writer.WriteString(yipname);
+                                writer.WriteEndElement();
+                                writer.WriteEndElement();
+                                writer.WriteEndElement();
+                                writer.WriteEndElement();
+
+                            }
+
+
+                            list.Add(newVsrPath);
+                            list.Add(newYipPath);
+                            list.Add(Path.Combine(archivePath, xmlname));
+
+
+                        }
+                        rowCounter++;
+                        lblRow.Text = (i-1).ToString();
+                        System.Windows.Forms.Application.DoEvents();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        SimpleLogger.SimpleLog.Log(ex); 
+                    }
+                }
+                var dest = Path.Combine(txtDest.Text, dirName);
+                Directory.CreateDirectory(dest);
+                foreach (var file in list)
+                {
+                    File.Move(file, Path.Combine(dest, Path.GetFileName(file)));
+                }
+                MessageBox.Show("הפעולה הסתיימה בהצלחה. " + rowCounter + " מתוך " + (lastRow - 1) + " נקלטו ");
+            }
+            catch (Exception ex)
+            {
+                SimpleLogger.SimpleLog.Log(ex);
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //rule of thumb for releasing com objects:
+                //  never use two dots, all COM objects must be referenced and released individually
+                //  ex: [somthing].[something].[something] is bad
+
+                //release com objects to fully kill excel process from running in the background
+                if (xlRange != null)
+                    Marshal.ReleaseComObject(xlRange);
+                if (xlWorksheet != null)
+                    Marshal.ReleaseComObject(xlWorksheet);
+
+                //close and release
+                if (xlWorksheet != null)
+                {
+                    xlWorkbook.Close();
+                    Marshal.ReleaseComObject(xlWorkbook);
+                }
+
+                if (xlApp != null)
+                {
+                    //quit and release
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                }
+
+            }
+        }
+        private string CreateName(string companyId, string id, string ext, NameType nameType)
+        {
+            var date = DateTime.Now.ToString("yyyyMMdd");
+            return "Sm" + id + "-SM-" + date + "-" + (nameType==NameType.PDF_VSR ? "VSR-" : nameType == NameType.PD_YIP ? "YIP-" : "")  + companyId + "." + ext;
+        }
+
+        private void butDest_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtDest.Text = fbd.SelectedPath;
+                    Properties.Settings.Default.DestPath = fbd.SelectedPath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void btnArchive_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtArchive.Text = fbd.SelectedPath;
+                    Properties.Settings.Default.ArchivePath = fbd.SelectedPath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
